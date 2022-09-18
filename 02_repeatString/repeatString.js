8c7777e27e75a7aea1d0122ab3286914343b9c38
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-09-18 12:07:39 -0500

    add solution to repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+let container = "";
+for(let i = 0; i < num; i++){
+    container += string
+}
+if(num < 0){
+    container = "ERROR"
+}
+return container;
 };
 
 // Do not edit below this line
