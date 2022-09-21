4cd6f9a2559a8db0b04306434bda12e2f28d6189
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-09-21 16:23:18 -0500

    add solution to round number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+// [°C] = ([°F] − 32) × 5⁄9
+// [°F] = [°C] × 9⁄5 + 32
+const ftoc = function(temp) {
+  let conversion = ""
+  conversion = (temp - 32) * (5/9);
+  return +conversion.toFixed(1)
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+let conversion = "";
+conversion = temp * (9/5) + 32;
 
+return +conversion.toFixed(1);
 };
 
 // Do not edit below this line
