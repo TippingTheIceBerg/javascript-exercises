8e6ae868694f1457e3082ffface4e7e34aa5c7c7
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-09-21 15:45:29 -0500

    add solution to leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+// years that can be divided by 4 are leap years, if they can be divided by 400 and 100 they are also leaps years. Otherwise they are not. By default, most years are not going to be leap years, so the default value should be false.
+const leapYears = function(year) {
+let results = false;
+
+if(year % 4 === 0){
+    results = true
+}
+if(year % 100 === 0 && year % 400 === 0){
+    results = true;
+}
+if(year % 100 === 0 && year % 400 !== 0){
+    results = false;
+}
+
 
 };
 
