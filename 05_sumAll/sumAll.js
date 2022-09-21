a842797e67b0a1dd6e180cae421f0311519107ae
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-09-20 19:07:05 -0500

    add solution to sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let startNum = 0;
+    let endNum = 0;
+    let sumNum = 0;
+    num1 > num2? endNum = num1:endNum = num2;
 
+for(let i = 0; i <= endNum; i++){
+    sumNum += i
+  
+}
+if(isNaN(num1) || isNaN(num2)){
+    sumNum = "ERROR"
+}
+if(typeof(num1) === "string" || typeof(num2) == "string"){
+    sumNum = "ERROR"
+}
+if(num1 < 0 || num2 < 0){
+    sumNum = "ERROR"
+}
+return sumNum;
 };
 
 // Do not edit below this line
