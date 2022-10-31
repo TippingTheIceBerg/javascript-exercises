c16d7c75c80717061fd6943025eaf575a1ab06dd
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-10-31 12:59:55 -0500

    solution to findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,29 @@
-const findTheOldest = function() {
+// First, we need to find the ages of the people.
+// With the ages of the people, find the person who's the oldest.
+//      A. If person is alive, use todays date
+// Return oldest person.
+// map it to find all ages, sort it to find the oldest, find to find.
+
+const findTheOldest = function(people) {
+
+people
+.map(e => {if(!e['yearOfDeath']){
+    e['yearOfDeath'] = new Date().getFullYear();
+}
+})
+
+let ageList = people
+.map(e=> e.age = e.yearOfDeath - e.yearOfBirth)
+.sort((a,b)=> a > b? 1:-1);
+
+let findOldest = people.find(name => (name.yearOfDeath-name.yearOfBirth) === ageList[ageList.length-1]);
+
+return findOldest;
+
+
+
+
+
 
 };
 
